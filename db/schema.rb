5af2d7c576c663547e41d430df079ee6feb751df
Cristobal Dominguez <cristobald@gmail.com>
2022-07-12 18:33:13 -0400

    Agrega Scaffold de Pet History

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_12_222954) do
+ActiveRecord::Schema.define(version: 2022_07_12_223240) do
+
+  create_table "pet_histories", force: :cascade do |t|
+    t.float "weight"
+    t.float "height"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "pets", force: :cascade do |t|
     t.string "name"
